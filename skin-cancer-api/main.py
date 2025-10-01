ae868ddb8653c8d30c33ca387242ec62a568d216
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2025-10-01 14:07:48 +0530

    CORS middleware added

--- skin-cancer-api/main.py
+++ skin-cancer-api/main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
 from predict import predict
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(title="Skin Cancer Detection API")
+
+# Define the list of allowed origins:(jaha se request aaaenge)
+origins = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://localhost:5500", # Common for local development
+    "http://127.0.0.1:5500", # Common for local development
+    "null",                  # Sometimes needed for local file:// access
+    # Add the URL of your deployed frontend app here once you have it
+    # e.g., "https://your-awesome-app.netlify.app", 
+]
+
+# Add the CORS middleware to the FastAPI application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
